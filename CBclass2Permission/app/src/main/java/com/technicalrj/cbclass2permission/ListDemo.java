2329dbf13efe37be753e16f0c434b7844c2fbff9
ashish <ashish4rawat@gmail.com>
2019-01-19 14:19:51 +0530

    Dangerous permisson

---
+++ CBclass2Permission/app/src/main/java/com/technicalrj/cbclass2permission/ListDemo.java
@@ -0,0 +1,16 @@
+package com.technicalrj.cbclass2permission;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_demo);
+
+
+
+    }
+}
--- CBclass2Permission/app/src/main/java/com/technicalrj/cbclass2permission/MainActivity.java
+++ CBclass2Permission/app/src/main/java/com/technicalrj/cbclass2permission/MainActivity.java
@@ -1,12 +1,20 @@
 package com.technicalrj.cbclass2permission;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkInfo;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import java.security.Permission;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+
+
+
 
     public void checkNetwork(View view) {
 
@@ -31,5 +44,21 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public void openFolder(View view) {
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.CALL_PHONE)==PackageManager.PERMISSION_GRANTED){
+            call();
+        }else {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},324);
+        }
+
+    }
+
+    public void call(){
+        Uri uri = Uri.parse("tel:8687263");
+        Intent intent = new Intent(Intent.ACTION_CALL,uri);
+        startActivity(intent);
     }
 }
